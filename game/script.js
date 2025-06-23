e9288bd6e6deca2a9c2e2b5a18f090a3c2a6bba0
W10-STI94-AIR\fbenedetti <f-s-benedetti@hotmail.com>
2025-06-23 13:59:11 -0300

    Creación carpeta game con index,style y script

--- game/script.js
+++ game/script.js
@@ -1,2 +1,21 @@
 const matrix = document.getElementById("matrix");
-matrix.textContent = "Hola";
+
+function generateMatrix(rows, columns) {
+    matrix.innerHTML = "";
+  
+    // Establecer dimensiones de la grilla
+    matrix.style.gridTemplateColumns = `repeat(${columns}, 20px)`;
+    matrix.style.gridTemplateRows = `repeat(${rows}, 20px)`;
+  
+    // Crear cuadrados
+    for (let i = 0; i < rows * columns; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      matrix.appendChild(square);
+    }
+  }
+
+// Ejemplo de uso
+generateMatrix(8, 8);
+
+

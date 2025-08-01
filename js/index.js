e6bfa35319a0e32a948ef4e93e87f7448fe72a7e
jeremias <jeremiasbarrile@gmail.com>
2025-07-31 22:45:15 -0300

    hacemos estilo deseado en index y el juego en si

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
-'use strict';
-
 document.addEventListener('DOMContentLoaded', function () {
   var userNameInput = document.getElementById('userName');
   var startButton = document.getElementById('startButton');
   var errorMessage = document.getElementById('errorMessage');
+  var leaderboardButton = document.getElementById('leaderboardButton');
+  var closeLeaderboardButton = document.getElementById('closeLeaderboardButton');
+  var leaderboard = document.getElementById('leaderboard');
 
+  // Mostrar leaderboard
+  leaderboardButton.addEventListener('click', function () {
+    leaderboard.classList.remove('hidden');
+  });
+
+  // Ocultar leaderboard
+  closeLeaderboardButton.addEventListener('click', function () {
+    leaderboard.classList.add('hidden');
+  });
+
+  // Validar nombre y redirigir a game.html
   startButton.addEventListener('click', function () {
     var name = userNameInput.value.trim();
 
